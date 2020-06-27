cf165a40584eb6d3c798dccef334c220ec3e3b4b
fomerna <fomerna@gmail.com>
2020-06-27 17:00:05 +0900

    Add: Initial Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "core-js/stable";
+
+console.log("a");
+
+document.getElementById("now-time").innerText = ''.concat(new Date().toString());
+
+const a = 10;
+console.log(a);
+
+let b = 20;
+console.log(b);
